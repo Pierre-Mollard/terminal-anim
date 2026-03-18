89a6b3c66b9e39ca54af1c502a2f139647678726
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2026-03-18 20:56:22 +0100

    feat: added lib context and dynamic buffers

--- src/terminal-handler.c
+++ src/terminal-handler.c
@@ -3,14 +3,34 @@
 #include "terminal-anim.h"
 #include <poll.h>
 #include <signal.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <sys/poll.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct tau_cell {
+  uint32_t symbol;
+  uint8_t fg_r, fg_g, fg_b;
+  uint8_t bg_r, bg_g, bg_b;
+  uint8_t attrs;
+};
+
 struct tau_ctx {
   int id;
+  struct tau_cell *front_buffer;
+  struct tau_cell *back_buffer;
+  size_t nb_cols;
+  size_t nb_rows;
+  size_t nb_cells;
+  size_t output_capacity;
+  char *output_buffer;
+  int cursor_x;
+  int cursor_y;
 };
+
+volatile int g_tau_ctx_nb_open = 0;
 
 volatile sig_atomic_t tau_g_is_running;
 static int has_init = 0;
@@ -18,7 +38,6 @@ static struct termios termios_conf_init;
 static unsigned int screen_max_rows, screen_max_cols = 0;
 
 tau_ctx *g_ctx_destroy_fallback = NULL;
-tau_ctx g_one_ctx_for_now = {1};
 
 void resize_screen_callback(int new_rows, int new_cols) {
   screen_max_rows = new_rows;
@@ -38,6 +57,8 @@ void tau_destroy(tau_ctx *ctx) {
   if (rc == 0) {
     rc = tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_conf_init);
   }
+
+  g_tau_ctx_nb_open--;
 }
 
 void generic_destroy() {
@@ -52,7 +73,34 @@ static void handler_sigs_end(int sig) {
 }
 
 tau_ctx *tau_create() {
-  tau_ctx *ctx = &g_one_ctx_for_now;
+  tau_ctx *ctx = calloc(1, sizeof(*ctx));
+  if (!ctx)
+    return NULL;
+
+  ctx->id = ++g_tau_ctx_nb_open;
+  ctx->cursor_x = 0;
+  ctx->cursor_y = 0;
+
+  // TODO: handle resize terminal that resize buffers
+  ctx->nb_rows = 20;
+  ctx->nb_cols = 20;
+  ctx->nb_cells = ctx->nb_rows * ctx->nb_cols;
+
+  ctx->back_buffer = calloc(ctx->nb_cells, sizeof(*ctx->back_buffer));
+  ctx->front_buffer = calloc(ctx->nb_cells, sizeof(*ctx->front_buffer));
+
+  // NOTE: output_buffer is 8bytes per cell
+  ctx->output_capacity = ctx->nb_cells * sizeof(*ctx->output_buffer) * 8;
+  ctx->output_buffer = calloc(ctx->output_capacity, ctx->output_capacity);
+
+  if (!ctx->back_buffer || !ctx->front_buffer || !ctx->output_buffer) {
+    free(ctx->back_buffer);
+    free(ctx->front_buffer);
+    free(ctx->output_buffer);
+    free(ctx);
+    return NULL;
+  }
+
   struct termios termios_conf_new;
   int rc = 0;
 
