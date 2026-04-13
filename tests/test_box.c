a8255b95117239800d6970a174215430db0caf79
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2026-04-13 20:00:43 +0200

    ivv: mouse event on edge resolution test

--- tests/test_box.c
+++ tests/test_box.c
@@ -1,12 +1,15 @@
 #include "terminal-anim.h"
 
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 tau_ctx *ctx;
 tau_style bg_style = {0};
+tau_style fg_red = {0};
 tau_style fg_style = {0};
 tau_style fg_light_style = {0};
 tau_style text_style = {0};
@@ -28,6 +31,11 @@ int main(int argc, char *argv[]) {
   fg_style.fg.r = 255;
   fg_style.has_fg = true;
 
+  fg_red.fg.r = 136;
+  fg_red.fg.g = 8;
+  fg_red.fg.b = 68;
+  fg_red.has_fg = true;
+
   fg_light_style.fg.b = 255;
   fg_light_style.has_fg = true;
 
@@ -41,27 +49,21 @@ int main(int argc, char *argv[]) {
   hv_lines_style.fg.g = 70;
   hv_lines_style.has_fg = true;
 
-  tau_fill(ctx, '.', bg_style);
   tau_draw_full(ctx);
 
   char *text = "box test";
-  tau_put_str(ctx, text, strlen(text), 2, 1, text_style);
 
   int x1 = 4;
   int x2 = 50;
   int y1 = 3;
   int y2 = 12;
 
-  tau_put_box(ctx, 10, 10, 10, 5, TAU_BOX_DOUBLE, fg_light_style);
-  tau_put_box(ctx, 21, 10, 10, 5, TAU_BOX_LIGHT, fg_style);
-  tau_put_box(ctx, 10, 21, 10, 5, TAU_BOX_HEAVY, hv_lines_style);
-
   tau_box box1 = {.x = 0,
                   .y = 2,
                   .width = 10,
                   .height = 5,
                   .box_style = TAU_BOX_LIGHT,
-                  .style = fg_light_style};
+                  .style = fg_red};
   tau_box box2 = {.x = 9,
                   .y = 2,
                   .width = 10,
@@ -77,10 +79,60 @@ int main(int argc, char *argv[]) {
 
   tau_box box_list[3] = {box1, box2, box3};
 
-  tau_put_box_grid(ctx, box_list, 3);
+  tau_toggle_input_evt(ctx, true);
 
-  tau_draw_diff(ctx);
-  sleep(5);
+  int counter = 0;
+  int evt_counter = 0;
+  int mouse_press_counter = 0;
+  int mouse_press_x = 0, mouse_press_y = 0;
+  char text_buffer[100];
+  tau_event evt = {0};
+  while (tau_g_is_running) {
+    struct timespec req;
+    req.tv_sec = 0;
+    req.tv_nsec = 20000000; // 20ms
+    struct timespec rem = {0};
+    counter++;
+
+    tau_update_input(ctx);
+    tau_update_resize(ctx);
+
+    bool has_event = tau_poll_event(ctx, &evt);
+    if (has_event) {
+      evt_counter++;
+      if (evt.type == TAU_EVT_MOUSE_PRESS) {
+        mouse_press_counter++;
+        mouse_press_x = evt.data.mouse.x;
+        mouse_press_y = evt.data.mouse.y;
+      }
+    }
+
+    box_list[0].x = mouse_press_x;
+    box_list[0].y = mouse_press_y;
+
+    tau_clear(ctx);
+
+    tau_put_str(ctx, text, strlen(text), 2, 1, text_style);
+    tau_put_box_grid(ctx, box_list, 3);
+
+    snprintf(text_buffer, sizeof(text_buffer), "[%d] any event counter : %d",
+             counter, evt_counter);
+    tau_put_str(ctx, text_buffer, 50, 100, 3, fg_style);
+
+    snprintf(text_buffer, sizeof(text_buffer),
+             "Mouse Event PRESS      : %d (%3d, %3d)", mouse_press_counter,
+             mouse_press_x, mouse_press_y);
+    tau_put_str(ctx, text_buffer, 50, 100, 6, fg_style);
+
+    tau_put_box(ctx, 10, 10 + 20, 10, 5, TAU_BOX_DOUBLE, fg_light_style);
+    tau_put_box(ctx, 21, 10 + 20, 10, 5, TAU_BOX_LIGHT, fg_style);
+    tau_put_box(ctx, 10, 12 + 20, 10, 5, TAU_BOX_HEAVY, hv_lines_style);
+    tau_draw_diff(ctx);
+    while (nanosleep(&req, &rem) == -1 && errno == EINTR) {
+      // retry with remaining time
+      req = rem;
+    }
+  }
 
   tau_destroy(ctx);
   ctx = NULL;
